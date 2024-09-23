4edcca900b6cd2dfd955d8486101ad783b8af169
Rohith Kodipaka <rohithkodipaka@Rohiths-MacBook-Air.local>
2024-09-23 08:58:50 -0500

    Project setup

---
+++ src/main/java/com/adbms/evaidhya/EvaidhyaApplication.java
@@ -0,0 +1,13 @@
+package com.adbms.evaidhya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaidhyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaidhyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adbms/evaidhya/EvaidhyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adbms.evaidhya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaidhyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
